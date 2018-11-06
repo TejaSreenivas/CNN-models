f0eba3f3462fa1defb30fce0cc62c403fa289996
TejaSreenivas <teja.sreenivas.05@gmail.com>
2018-11-06 17:49:09 +0530

    architecture update

--- new_model.py
+++ new_model.py
@@ -40,11 +40,11 @@ class CNN:
             conv1 = self.create_conv_layer(data, l1, [1]*4, "SAME")
             conv1 = tf.layers.batch_normalization(conv1, training = is_train)
             conv1 = tf.nn.relu(conv1)
-            #conv1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides = [1,2,2,1], padding = "SAME")
+            conv1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides = [1,2,2,1], padding = "SAME")
             #conv1 = tf.layers.dropout(conv1, rate = prob_keep, training = is_train)
         l2 = [5,5,l1[-1],64]
         with tf.variable_scope("conv_2", reuse=tf.AUTO_REUSE):
-            conv2 = self.create_conv_layer(conv1, l2, [1,2,2,1], "VALID")
+            conv2 = self.create_conv_layer(conv1, l2, [1]*4, "SAME")
             #conv2 = tf.layers.batch_normalization(conv2, training = is_train)
             conv2 = tf.nn.relu(conv2)
             conv2 = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides = [1,2,2,1], padding = "SAME")
@@ -55,8 +55,8 @@ class CNN:
             conv3 = self.create_conv_layer(conv2, l3, [1]*4, "SAME")
             #conv3 = tf.layers.batch_normalization(conv3, training = is_train)
             conv3 = tf.nn.relu(conv3)
-            #conv3 = tf.nn.max_pool(conv3, ksize=[1,2,2,1], strides = [1,2,2,1], padding = "SAME")
-            conv3 = tf.layers.dropout(conv3, rate = prob_keep, training = is_train)
+            conv3 = tf.nn.max_pool(conv3, ksize=[1,2,2,1], strides = [1,2,2,1], padding = "SAME")
+            #conv3 = tf.layers.dropout(conv3, rate = prob_keep, training = is_train)
         """
         l4 = [3,3,l3[-1],256]
         with tf.variable_scope("conv_4", reuse=tf.AUTO_REUSE):
@@ -66,7 +66,7 @@ class CNN:
             conv4 = tf.nn.max_pool(conv4, ksize=[1,2,2,1], strides = [1,2,2,1], padding = "SAME")
             conv4 = tf.layers.dropout(conv4, rate = prob_keep, training = is_train)
         """
-        flat = tf.reshape(conv3,[-1,7*7*64])
+        flat = tf.layers.flatten(conv3)
         bn1 = tf.layers.batch_normalization(inputs=flat,training = is_train)
         h1 = tf.layers.dense(bn1, units=1024)
         h1 = tf.layers.batch_normalization(inputs=h1, training = is_train)
